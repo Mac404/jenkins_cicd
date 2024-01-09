2ad9c028f0c6a645f344b6fc2d3939bd0faf45f7
Valerio <78894056+ulissesss@users.noreply.github.com>
2024-01-09 13:46:56 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+resource "aws_instance" "public_instance" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  
+  tags = {
+    Name = var.name_tag,
+  }
+  
+  key_name = aws_key_pair.autodeploy.key_name  # Link the key pair to the instance
+}
+
 #Create security group 
 resource "aws_security_group" "jenkins_sg" {
   name        = "jenkins_sg"
@@ -13,25 +24,8 @@ resource "aws_security_group" "jenkins_sg" {
   }
 }
 
-resource "aws_instance" "public_instance" {
-  ami           = var.ami
-  instance_type = var.instance_type
- # security_groups  = var.sg
-  
-  tags = {
-    Name = var.name_tag,
-  }
-  
-  key_name = aws_key_pair.autodeploy.key_name  # Link the key pair to the instance
-}
-
 resource "aws_key_pair" "autodeploy" {
   key_name   = "autodeploy"  # Set a unique name for your key pair
   public_key = file("/var/jenkins_home/.ssh/id_rsa.pub")
 }
-
-
-
-
-
 
