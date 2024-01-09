12052fe7dfc743ef358e4ecec41ed07e34093500
Valerio <78894056+ulissesss@users.noreply.github.com>
2024-01-09 13:33:50 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+#Create security group 
+resource "aws_security_group" "jenkins_sg" {
+  name        = "jenkins_sg"
+  description = "Open ports 22"
+
+  #Allow incoming TCP requests on port 22 from any IP
+  ingress {
+    description = "Incoming SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["69.42.6.44/32"]
+  }
+}
+
 resource "aws_key_pair" "autodeploy" {
   key_name   = "autodeploy"  # Set a unique name for your key pair
   public_key = file("/var/jenkins_home/.ssh/id_rsa.pub")
@@ -15,19 +30,6 @@ resource "aws_instance" "public_instance" {
   key_name = aws_key_pair.autodeploy.key_name  # Link the key pair to the instance
 }
 
-#Create security group 
-resource "aws_security_group" "jenkins_sg" {
-  name        = "jenkins_sg"
-  description = "Open ports 22"
 
-  #Allow incoming TCP requests on port 22 from any IP
-  ingress {
-    description = "Incoming SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["69.42.6.44/32"]
-  }
-}
 
 
