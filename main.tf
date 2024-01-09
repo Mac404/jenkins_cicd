6c137f1dfa38ef80cfad3f397c680fe95f071eb9
Valerio <78894056+ulissesss@users.noreply.github.com>
2024-01-09 15:03:12 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "public_instance" {
   ami           = var.ami
   instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group-jenkins_sg]
+  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
   
   tags = {
     Name = var.name_tag,
