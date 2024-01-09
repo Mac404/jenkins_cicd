e5fae7f450669ec3dc50353082435afda20a3ce9
Valerio <78894056+ulissesss@users.noreply.github.com>
2024-01-09 15:00:49 -0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -34,8 +34,4 @@ variable "name_tag" {
    description = "Name of the EC2 instance"
    default     = "My EC2 Instance"
 }
-#variable "sg" {
-  # type        = string
-  # description = "security group"
-  # default     = "jenkins_sg"
-#}
+
