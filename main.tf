d936f617316660b5c5fd1a298cdb3ab609cc25ad
Valerio <78894056+ulissesss@users.noreply.github.com>
2024-01-09 23:44:08 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -50,6 +50,8 @@ resource "aws_volume_attachment" "ebs" {
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+  instance_tenancy = "default"
+  enable_dns_hostnames = "true"
 
   tags = {
     Name = "${var.vpc_name}-VPC"
