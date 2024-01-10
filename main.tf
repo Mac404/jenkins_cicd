f72fe6b49bd3d8e4e679b47ec1b50a6018d23b10
Valerio <78894056+ulissesss@users.noreply.github.com>
2024-01-09 20:14:47 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -46,3 +46,35 @@ resource "aws_volume_attachment" "ebs" {
   instance_id = aws_instance.public_instance.id
 }
 
+resource "aws_vpc" "main" {
+ cidr_block = "10.0.0.0/16"
+ 
+ tags = {
+   Name = "Project VPC"
+ }
+}
+
+resource "aws_subnet" "public_subnets" {
+ count      = length(var.public_subnet_cidrs)
+ vpc_id     = aws_vpc.main.id
+ cidr_block = element(var.public_subnet_cidrs, count.index)
+ 
+ tags = {
+   Name = "Public Subnet ${count.index + 1}"
+ }
+}
+ 
+resource "aws_subnet" "private_subnets" {
+ count      = length(var.private_subnet_cidrs)
+ vpc_id     = aws_vpc.main.id
+ cidr_block = element(var.private_subnet_cidrs, count.index)
+ 
+ tags = {
+   Name = "Private Subnet ${count.index + 1}"
+ }
+}
+
+
+
+
+
