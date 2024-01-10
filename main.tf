4dd59deca336e65a554cd470c16827ed473237d4
Valerio <78894056+ulissesss@users.noreply.github.com>
2024-01-09 22:38:32 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -64,7 +64,7 @@ resource "aws_internet_gateway" "gw" {
 }
 
 resource "aws_subnet" "public_subnets" {
-  count             = length(var.azs)
+  count             = length(var.public_subnet_cidrs)
   vpc_id            = aws_vpc.main.id
   cidr_block        = element(var.public_subnet_cidrs, count.index)
 
@@ -74,7 +74,7 @@ resource "aws_subnet" "public_subnets" {
 }
 
 resource "aws_subnet" "private_subnets" {
-  count             = length(var.azs)
+  count             = length(var.private_subnet_cidrs)
   vpc_id            = aws_vpc.main.id
   cidr_block        = element(var.private_subnet_cidrs, count.index)
 
