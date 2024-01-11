ed480e2f7ac4ce57949407affce564f89703312b
Valerio <78894056+ulissesss@users.noreply.github.com>
2024-01-11 11:30:16 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -123,7 +123,7 @@ resource "aws_route_table_association" "private_subnet_asso" {
 
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "mybucket3445345656457676878687867867867"
+  bucket = "mybucket_val_3445345656457676878687867867867"
   acl    = "private"
   force_destroy = true
   lifecycle {
