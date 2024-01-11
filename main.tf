0041ade0afe044bbd69f8149c69e2a31269b3806
Valerio <78894056+ulissesss@users.noreply.github.com>
2024-01-10 18:05:08 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,14 @@ resource "aws_instance" "public_instance" {
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
   subnet_id = "${aws_subnet.public_subnets.id}"
-    
+  user_data = <<-EOF
+  #!/bin/bash
+  echo "*** Installing apache2"
+  sudo apt update -y
+  sudo apt install apache2 -y
+  echo "*** Completed Installing apache2"
+  EOF
+ 
   tags = {
     Name = var.name_tag,
   }
@@ -25,9 +32,9 @@ resource "aws_security_group" "jenkins_sg" {
   #Allow incoming TCP requests on port 22 from any IP
   ingress {
     description = "Incoming SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["69.42.6.44/32" , "98.42.124.215/32", "192.168.1.175/32" ]
   }
 # Internet access to anywhere
@@ -114,33 +121,4 @@ resource "aws_route_table_association" "private_subnet_asso" {
   subnet_id = "${aws_subnet.private_subnets.id}"
   route_table_id = "${aws_route_table.subnets.id}"
 }
-
-resource "aws_instance" "web" {
-  ami             = var.ami
-  instance_type =  var.instance_type
-  key_name = aws_key_pair.autodeploy.key_name 
-  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
-  subnet_id = "${aws_subnet.public_subnets.id}"
-
-  user_data = <<-EOF
-  #!/bin/bash
-  echo "*** Installing apache2"
-  sudo apt update -y
-  sudo apt install apache2 -y
-  echo "*** Completed Installing apache2"
-  EOF
-
-  tags = {
-    Name = "web_instance"
-  }
-
-  volume_tags = {
-    Name = "web_instance"
-  } 
-}
-
-
-
-
-
 
