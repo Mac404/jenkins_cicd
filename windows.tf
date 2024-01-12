18e3cd349315b753482300cd70d952c62b80a862
Valerio <78894056+ulissesss@users.noreply.github.com>
2024-01-11 19:00:20 -0800

    Create windows.tf

---
+++ windows.tf
@@ -0,0 +1,32 @@
+resource "aws_instance" "windows-server" {
+     ami = data.aws_ami.windows.id
+     instance_type = var.instance_type
+     availability_zone = var.availability_zone
+     vpc_security_group_ids    = [aws_security_group.jenkins_sg.id]
+     key_name = var.aws_key_name
+
+ tags = {
+    Name        = "${lower(var.app_name)}-${var.app_environment}-windows-server"
+    Environment = var.app_environment
+  }
+
+ }
+
+# Generates a secure private key and encodes it as PEM
+resource "tls_private_key" "key_pair" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# Create the Key Pair
+resource "aws_key_pair" "key_pair" {
+  key_name   = "${lower(var.app_name)}-${lower(var.app_environment)}-windows-${lower(var.region)}"  
+  public_key = tls_private_key.key_pair.public_key_openssh
+}
+
+# Save file
+resource "local_file" "ssh_key" {
+  filename = "${aws_key_pair.key_pair.key_name}.pem"
+  content  = tls_private_key.key_pair.private_key_pem
+}
+
