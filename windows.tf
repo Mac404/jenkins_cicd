6f1e8abd8a948d7242d28c713cb3839ef7754073
Valerio <78894056+ulissesss@users.noreply.github.com>
2024-01-11 20:28:50 -0800

    Update windows.tf

--- windows.tf
+++ windows.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "windows-server" {
      ami = var.win_ami
      instance_type = var.instance_type
      vpc_security_group_ids    = [aws_security_group.windows.id]
-     key_name = var.aws_key_name
+     key_name = "mykey"
 
  tags = {
     Name        = "${lower(var.app_name)}-${var.app_environment}-windows-server"
