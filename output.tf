6188fe4973a64266dd3fd700d8997e7b3ec16d08
Valerio <78894056+ulissesss@users.noreply.github.com>
2024-01-12 12:16:24 -0800

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,17 +1,1 @@
-output "public_ip" {
- value       = aws_instance.public_instance.public_ip
- description = "Public IP Address of EC2 instance"
-}
 
-output "instance_id" {
- value       = aws_instance.public_instance.id
- description = "Instance ID"
-}
-
-output "volume_id" {
-  value = aws_ebs_volume.st1.id
-}
-
-output "attachment_id" {
-  value = aws_volume_attachment.ebs.id
-}
